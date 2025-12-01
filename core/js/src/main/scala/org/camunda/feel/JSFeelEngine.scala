79de5696aa8399ea33e10734a7f9b4f3f4f05fc2
Carlo Sana <carlo.sana@camunda.com>
2025-12-01 22:46:44 +0100

    feat(playground): use scalajs feel engine instead of using API

--- core/js/src/main/scala/org/camunda/feel/JSFeelEngine.scala
+++ core/js/src/main/scala/org/camunda/feel/JSFeelEngine.scala
@@ -1,15 +1,17 @@
 package org.camunda.feel
 
-import org.camunda.feel.api.FeelEngineApi
+import org.camunda.feel.api.{
+  EvaluationFailure,
+  EvaluationResult,
+  FailedEvaluationResult,
+  FeelEngineApi,
+  SuccessfulEvaluationResult
+}
 
 import scala.scalajs.js
-import scala.scalajs.js.annotation.{JSExportTopLevel, JSExportAll}
+import js.JSConverters._
+import scala.scalajs.js.annotation.{JSExportAll, JSExportTopLevel}
 import org.camunda.feel.impl.interpreter.ObjectContext
-import org.camunda.feel.api.SuccessfulEvaluationResult
-import org.camunda.feel.api.FailedEvaluationResult
-import org.camunda.feel.api.EvaluationResult
-import org.camunda.feel.valuemapper.ValueMapper
-import org.camunda.feel.impl.DefaultValueMapper
 import org.camunda.feel.valuemapper.JSValueMapper
 
 @JSExportAll
@@ -21,13 +23,31 @@ class JSFeelEngine() {
 
   private val api = new FeelEngineApi(engine)
 
-  def evaluate(expression: String, context: js.Dynamic): EvaluationResult = {
+  def evaluate(expression: String, context: js.Dynamic): js.Dynamic = {
     val ctx = ObjectContext(context)
-    api.evaluateExpression(expression, ctx)
+    api.evaluateExpression(expression, ctx) match {
+      case SuccessfulEvaluationResult(result, suppressedFailures) =>
+        js.Dynamic.literal(
+          result = result.asInstanceOf[js.Any],
+          isSuccess = true,
+          warnings = suppressedFailures.map(convertWarning).toJSArray
+        )
+      case FailedEvaluationResult(failure, suppressedFailures)    =>
+        js.Dynamic.literal(
+          failure = failure.message,
+          isSuccess = false,
+          warnings = suppressedFailures.map(convertWarning).toJSArray
+        )
+    }
   }
 
   def evaluateUnaryTests(expression: String, context: js.Dynamic): EvaluationResult = {
     val ctx = ObjectContext(context)
     api.evaluateUnaryTests(expression, ctx)
   }
+
+  def convertWarning(failure: EvaluationFailure): js.Dynamic = {
+    js.Dynamic.literal(message = failure.failureMessage, `type` = failure.failureType.toString)
+  }
+
 }
